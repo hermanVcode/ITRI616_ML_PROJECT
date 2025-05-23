defb67a6e06bdd5fb9902be822d8d8f5f633139e
hermanVcode <herman.vanheerden@hotmail.com>
2025-05-23 12:10:16 +0200

    Created new python script and adjusted notebook

---
+++ python_scripts/preprocessing_image_size.py
@@ -0,0 +1,5 @@
+# For resizing the images from default 178*218 pixels to 128*128 while preserving facial proportions
+# Remove background from images
+
+# Step 1: Crop and then center image as square (178*178)
+# Step 2: Resize from 178*178 to 128*128
